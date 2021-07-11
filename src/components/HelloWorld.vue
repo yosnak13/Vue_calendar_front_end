b2b22908b10b6f2a1a3231b57a1283e626dfe75b
yosnak13 <yosnak13@gmail.com>
2021-07-11 17:17:57 +0900

    v-if文の練習

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,19 @@
 <template>
-  <p>Hello World!</p>
+  <div v-if="visibility">
+    <p>visibility: true</p>
+    {{ message }}
+  </div>
+  <div v-else>
+    <p>visibility: false</p>
+  </div>
 </template>
 
 <script>
   export default {
     name: "HelloWorld",
+    data: () =>  ({
+      message: "Hello world",
+      visibility: true,
+    }),
   };
 </script>
