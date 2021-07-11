095058c56036497b447b31546e569ecd51baaea4
yosnak13 <yosnak13@gmail.com>
2021-07-11 17:44:37 +0900

    クリックイベント

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,16 @@
 <template>
   <div>
-    <ul>
-      <li v-for="item in items" :key="item.id">
-        {{ "id" + item.id }}
-        {{ item.text }}
-      </li>
-    </ul>
+    <button @click="clickedEvent()">ボタン</button>
   </div>
 </template>
 
 <script>
   export default {
     name: "HelloWorld",
-    data: () =>  ({
-      items: [
-        { id: 1, text: 'アイテム1' },
-        { id: 2, text: 'アイテム2' },
-        { id: 3, text: 'アイテム3' }
-      ],
-    }),
+    methods: {
+      clickedEvent(){
+        console.log('clicked!');
+      }
+    },
   };
 </script>
