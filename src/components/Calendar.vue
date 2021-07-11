4db8a53c3abe7b88dfa3b1acc299026c257015e5
yosnak13 <yosnak13@gmail.com>
2021-07-12 07:12:08 +0900

    axiosでAPIを呼び出してデータを取得

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,14 +3,27 @@
     <h1>Calendar</h1>
     <p>events:</p>
     <p>{{ events }}</p>
-    <button type="submit">fetchEvents</button>
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "Calendar",
   data: () => ({
     events: [],
   }),
+  methods: {
+    fetchEvents() {
+      axios
+        .get("http://localhost:3000/events")
+        .then(response => {
+          this.events = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  },
 };
 </script>
