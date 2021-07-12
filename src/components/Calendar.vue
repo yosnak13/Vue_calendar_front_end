da9385fab2f1710ea1e2a5a2934bc4d8c6cd104c
yosnak13 <yosnak13@gmail.com>
2021-07-12 22:25:44 +0900

    CalendarDetailコンポーネントを呼び出して表示

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <h1>Calendar</h1>
-    <p>events:</p>
-    <p>{{ events }}</p>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        {{ event.name }}
+      </li>
+    </ul>
     <button type="submit" @click="fetchEvents()">fetchEvents</button>
+    <CalendarDetails />
   </div>
 </template>
+
 <script>
 import axios from "axios";
+import CalendarDetails from './CalendarDetails.vue';
+
 export default {
   name: "Calendar",
+  components: {
+    CalendarDetails,
+  },
   data: () => ({
     events: [],
   }),
@@ -17,10 +27,10 @@ export default {
     fetchEvents() {
       axios
         .get("http://localhost:3000/events")
-        .then(response => {
+        .then((response) => {
           this.events = response.data;
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     },
