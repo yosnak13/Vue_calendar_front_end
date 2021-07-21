3e20807436b6a8a648dace0ecb08ffabf7a46fe4
yosnak13 <yosnak13@gmail.com>
2021-07-21 23:48:50 +0900

    とりあえず映る

--- src/store/modules/events.js
+++ src/store/modules/events.js
@@ -2,19 +2,20 @@ import axios from "axios";
 const apiUrl = "http://localhost:3000";
 
 const state = {
+  events: [],
   event: null,
 };
 
 const getters = {
   //変換処理
-  // events: (state) =>
-  //   state.events.map((event) => {
-  //     return {
-  //       ...event,
-  //       start: new Date(event.start),
-  //       end: new Date(event.end),
-  //     };
-  //   }),
+  events: (state) =>
+    state.events.map((event) => {
+      return {
+        ...event,
+        start: new Date(event.start),
+        end: new Date(event.end),
+      };
+    }),
   event: (state) =>
     state.event
       ? {
@@ -26,13 +27,14 @@ const getters = {
 };
 
 const mutations = {
+  setEvents: (state, events) => (state.events = events),
   setEvent: (state, event) => (state.event = event),
 };
 
 const actions = {
   async fetchEvents({ commit }) {
     const response = await axios.get(`${apiUrl}/events`);
-    commit("setEvent", response.data); // mutationを呼び出す
+    commit("setEvents", response.data); // mutationを呼び出す
   },
   setEvent({ commit }, event) {
     commit("setEvent", event);
