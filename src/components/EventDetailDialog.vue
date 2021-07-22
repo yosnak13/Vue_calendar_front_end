1897070c3927d08e4262f93d3341d2ac765fa8b6
yosnak13 <yosnak13@gmail.com>
2021-07-22 14:18:20 +0900

    DialogSectionコンポーネントの作成

--- src/components/EventDetailDialog.vue
+++ src/components/EventDetailDialog.vue
@@ -7,36 +7,19 @@
     </v-card-actions>
 
     <v-card-title>
-      <v-row>
-        <v-col cols="2" class="d-flex justify-center align-center">
-          <v-icon size="20px" :color="event.color || 'blue'">mdi-square</v-icon>
-        </v-col>
-        <v-col class="d-flex align-center">
-          {{ event.name }}
-        </v-col>
-      </v-row>
+      <DialogSection icon="mdi-square" :color="event.color || 'bule'">
+        {{ event.name }}
+      </DialogSection>
     </v-card-title>
-
     <v-card-text>
-      <v-row>
-        <v-col cols="2" class="d-flex justify-center align-center">
-          <v-icon size="20px">mdi-clock-time-three-outline</v-icon>
-        </v-col>
-        <v-col class="d-flex align-center">
-          {{ event.start.toLocaleString() }} ~ {{ event.end.toLocaleString() }}
-        </v-col>
-      </v-row>
+      <DialogSection icon="mdi-clock-time-three-outline">
+        {{ event.start.toLocaleString() }} ~ {{ event.end.toLocaleString() }}
+      </DialogSection>
     </v-card-text>
-
     <v-card-text>
-      <v-row>
-        <v-col cols="2" class="d-flex justify-center align-center">
-          <v-icon size="20px">mdi-card-text-outline</v-icon>
-        </v-col>
-        <v-col class="d-flex align-center">
-          {{ event.description || 'no description' }}
-        </v-col>
-      </v-row>
+      <DialogSection icon="mdi-card-text-outline">
+        {{ event.description || 'no description' }}
+      </DialogSection>
     </v-card-text>
 
   </v-card>
@@ -44,9 +27,13 @@
 
 <script>
 import { mapGetters, mapActions } from'vuex'
+import DialogSection from './DialogSection';
 
 export default {
   name: 'EventDetailDialog',
+  components: {
+    DialogSection,
+  },
   computed: {
     ...mapGetters('events', ['event']),
   },
