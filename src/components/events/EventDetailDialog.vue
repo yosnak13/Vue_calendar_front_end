acadcfe9a4d844a18c0d92791ca9c5d890dc2842
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-08-11 10:11:50 +0900

    componentsディレクトリを整理し、importのパスを変更

--- src/components/events/EventDetailDialog.vue
+++ src/components/events/EventDetailDialog.vue
@@ -33,7 +33,7 @@
 
 <script>
 import { mapGetters, mapActions } from'vuex'
-import DialogSection from './DialogSection';
+import DialogSection from '../layouts/DialogSection';
 
 export default {
   name: 'EventDetailDialog',
